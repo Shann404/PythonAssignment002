42bc97267f0e7f1fae33615ee7c0d79c8080f10e
Shann404 <shannbahati@gmail.com>
2025-08-05 23:09:11 +0300

    Lists assignment

---
+++ lists.py
@@ -0,0 +1,31 @@
+# creating empty list called my_list
+my_list=[]
+
+# appending values to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print (my_list)
+
+# inserting 15 at second position
+my_list.insert(1, 15)
+print(my_list)
+
+# extending my_list with another list
+other_list=[50,60,70]
+my_list.extend(other_list)
+print(my_list)
+
+# removing last elent from my_list
+my_list.pop()
+print(my_list)
+
+# sorting my_list in ascending order
+my_list.sort()
+print(my_list)
+
+# printing value at index 30
+index_30=my_list.index(30)
+print(index_30)
+
